8972859cf53c993eef29034f8578026fd74cb9d4
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-24 15:09:05 -0400

    adds horoscope struct

---
+++ UserDefaults-Lab/Models/Horoscope.swift
@@ -0,0 +1,36 @@
+//
+//  Horoscope.swift
+//  UserDefaults-Lab
+//
+//  Created by Angela Garrovillas on 9/24/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
+struct Horoscope: Codable {
+    let sunsign: String
+    let horoscope: String
+    let meta: Meta
+    
+    static func getHoroscope(url: URL, completionHandler: @escaping (Result<Horoscope,AppError>) -> ()) {
+        NetworkHelper.manager.performDataTask(withUrl: url, andMethod: HTTPMethod.get) { (result) in
+            switch result {
+            case .failure(let error):
+                completionHandler(.failure(error))
+            case .success(let data):
+                do {
+                let horoscopeFromJSON = try JSONDecoder().decode(Horoscope.self, from: data)
+                completionHandler(.success(horoscopeFromJSON))
+                } catch {
+                    completionHandler(.failure(.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+    }
+}
+
+struct Meta: Codable {
+    let mood: String
+    let keywords: String
+    let intensity: String
+}
---
+++ UserDefaults-Lab/Models/HoroscopeAPIHelper.swift
@@ -0,0 +1,9 @@
+//
+//  HoroscopeAPIHelper.swift
+//  UserDefaults-Lab
+//
+//  Created by Angela Garrovillas on 9/24/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
